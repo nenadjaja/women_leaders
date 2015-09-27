cad046f5924a9c828d0f2ddccdc32cc7edecb8c9
Nevena Djaja <nevena.djaja@gmail.com>
2015-09-27 15:51:43 -0700

    initial commit

---
+++ models/database.js
@@ -0,0 +1,12 @@
+var pg = require('pg');
+var connectionString = process.env.DATABASE_URL || "postgres://localhost:5432/leaders";
+
+// create a new postgres client and connect to it
+var client = new pg.Client(connectionString);
+client.connect();
+
+// create a new table in the database leaders
+var createQuery = client.query('CREATE TABLE women(id SERIAL PRIMARY KEY, name VARCHAR(30) not null, title VARCHAR(30), img TEXT)');
+createQuery.on('end', function() {
+	client.end();
+});
